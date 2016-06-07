6af0339cf895ccfe2f008a2b8a7a2b36f1f83c0c
plex <plex@peter@voxxse.com>
2016-06-07 16:25:41 -0500

    lights

--- mauris.js
+++ mauris.js
@@ -1427,10 +1427,12 @@ function preload () {
 				game.load.audio('powerup','assets/powerup.wav');
 				game.load.audio('missile','assets/missile.wav');
 				game.load.audio('bullet','assets/bullet.wav');
-//								game.load.audio('1','assets/1.ogg');
-//							game.load.audio('2','assets/2.ogg');
-//						game.load.audio('3','assets/3.ogg');
-//					game.load.audio('4','assets/4.ogg');
+if(!isAndroid){
+								game.load.audio('1','assets/1.ogg');
+							game.load.audio('2','assets/2.ogg');
+						game.load.audio('3','assets/3.ogg');
+					game.load.audio('4','assets/4.ogg');
+}
 }
 
 
@@ -2490,6 +2492,8 @@ playerShip.prototype.update = function(){
 				}
 };
 var touchPressed = 0;
+var isAndroid = /(android)/i.test(navigator.userAgent);
+
 var player;
 var currentBrightness=1.0;
 var mysteriousConstant=0.22;
@@ -2668,14 +2672,18 @@ gameUI.prototype.initSound = function(){
 				this.sound_bullet = game.add.audio('bullet');
 				this.currentMusic=undefined;
 				this.music=[];
-		/*		for(var i = 1; i <=4; i++){
+if(!isAndroid){
+for(var i = 1; i <=4; i++){
 													this.music[i]=game.add.audio(i);
-				}*/
+}
+}
 }
 function checkForNewMusic(){
-/*	if(game.time.now>nextMusic){
+if(!isAndroid){
+if(game.time.now>nextMusic){
 		ui.music_random();
-	}*/
+}
+}
 }
 gameUI.prototype.music_random = function(){
 				var rnd = parseInt(randomRange(1,this.music.length));
