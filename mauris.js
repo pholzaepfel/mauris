f7243eaec53dba26be400989a926f01704057e2d
plex <plex@peter@voxxse.com>
2018-02-24 06:26:57 -0600

    .

--- mauris.js
+++ mauris.js
@@ -124,7 +124,7 @@ var randomMission = function(){
 								var count = parseInt(randomRange(3,7));
 								var strength = count;
 								var roll = 0;
-								var loops = randomRange(0,level/2);
+								var loops = randomRange(0,playerStats.mission.level/2);
 								for(var i=0;i<loops;i++){
 								roll = randomRange(0,50);
 								if(roll < 10){
