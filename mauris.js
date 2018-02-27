4fb2d1a405fd92e04dc09a77aff8db274e83f327
plex <plex@peter@voxxse.com>
2018-02-26 21:00:04 -0600

    .

--- mauris.js
+++ mauris.js
@@ -1884,15 +1884,8 @@ enemyShip.prototype.update = function() {
 																this.rightCooldown = game.time.now + 200;
 												}else if(diffAngle*60<-this.turnRate && game.time.now > this.rightCooldown){
 																this.right(1);
-												}else if(diffAngle>0)
-																{
-																				this.left(diffAngle/(game.math.degToRad(this.turnRate)*shipSpeed*game.time.physicsElapsed      ));  
-																}else if(diffAngle<0
-																				){
-																				this.right(diffAngle/(game.math.degToRad(this.turnRate)*shipSpeed*game.time.physicsElapsed * -1));
-																}
-
-
+																this.leftCooldown = game.time.now + 200;
+												}
 
 
 
@@ -3090,17 +3083,11 @@ playerShip.prototype.update = function(){
 												if(diffAngle*60>this.turnRate && !touchPressed && game.time.now > this.leftCooldown)
 												{
 																this.left(1);
+																this.rightCooldown = game.time.now + 100000;
 												}else if(diffAngle*60<-this.turnRate && !touchPressed && game.time.now > this.rightCooldown){
 																this.right(1);
-												}else if(diffAngle>0)
-																{
-																				this.left(diffAngle/(game.math.degToRad(this.turnRate)*shipSpeed*game.time.physicsElapsed      ));  
-																}else if(diffAngle<0
-																				){
-																				this.right(diffAngle/(game.math.degToRad(this.turnRate)*shipSpeed*game.time.physicsElapsed * -1));
-																}
-
-
+																this.leftCooldown = game.time.now + 100000;
+												}
 												if(game.input.activePointer.isDown && !touchPressed && Math.abs(diffAngle) < 0.2){
 																this.up(1);
 												}
@@ -3148,17 +3135,11 @@ x: this.target.x + this.target.body.velocity.x * timeToImpact ,
 																if(diffAngle*60>this.turnRate && game.time.now)
 																{
 																				this.left(1);
+																				this.rightCooldown = game.time.now + 10000;
 																}else if(diffAngle*60<-this.turnRate && game.time.now){
 																				this.right(1);
-																}else if(diffAngle>0)
-																{
-																				this.left(diffAngle/(game.math.degToRad(this.turnRate)*shipSpeed*game.time.physicsElapsed      ));  
-																}else if(diffAngle<0
-																				){
-																				this.right(diffAngle/(game.math.degToRad(this.turnRate)*shipSpeed*game.time.physicsElapsed * -1));
+																				this.leftCooldown = game.time.now + 10000;
 																}
-
-
 																if(Math.abs(diffAngle) < 0.05){
 																				this.up(1);
 																}
