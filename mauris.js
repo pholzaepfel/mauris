01fab2e569abc877b7bf847ce511a223cf151bad
plex <plex@peter@voxxse.com>
2018-02-27 07:11:37 -0600

    .

--- mauris.js
+++ mauris.js
@@ -78,7 +78,7 @@ var randomMission = function(){
 				rm.hazeRedSpeed = randomRange(20,40);
 				rm.hazeWhiteSpeed = 10;
 				rm.hazePurpleSpeed = randomRange(60,100);
-				if(randomRange(0,1)<0.1 || isAndroid){
+				if(randomRange(0,1)<0.6 || isAndroid){
 								rm.distantPlanet = true;
 								rm.hazeWhiteSpeed=2;
 				}
