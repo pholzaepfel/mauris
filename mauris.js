8b2dc52586e9d8ebae79ce65830e363010b1a13b
plex <plex@peter@voxxse.com>
2018-03-12 22:30:41 -0500

    ...

--- mauris.js
+++ mauris.js
@@ -5901,7 +5901,7 @@ function update () {
 									if(wasUp){
 										player.nextFire-=300;
 										if(player.energy<player.energyMax){
-										player.energy+=2;
+										player.energy+=1;
 										}
 										player.sprite.profile+=100;
 										wasUp=false;
